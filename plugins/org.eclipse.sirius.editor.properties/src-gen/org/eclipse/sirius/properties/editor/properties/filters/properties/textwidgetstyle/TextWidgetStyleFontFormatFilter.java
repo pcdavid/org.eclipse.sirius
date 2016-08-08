ee35081ebd08b1e627c4ec352a6f4c1d0cb39f0e
Pierre-Charles David <pierre-charles.david@obeo.fr>
2016-08-08 13:43:38 +0200

    Plain regen of filters (no after-regen change)
    Change-Id: I44fbaf424e06b68ed6f294294a964a96282c5440

---
+++ plugins/org.eclipse.sirius.editor.properties/src-gen/org/eclipse/sirius/properties/editor/properties/filters/properties/textwidgetstyle/TextWidgetStyleFontFormatFilter.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.properties.editor.properties.filters.properties.textwidgetstyle;
+
+// Start of user code specific imports
+
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.sirius.properties.PropertiesPackage;
+import org.eclipse.sirius.properties.editor.properties.filters.common.ViewpointPropertyFilter;
+
+// End of user code specific imports
+
+
+/**
+ * A filter for the fontFormat property section.
+ */
+public class TextWidgetStyleFontFormatFilter extends ViewpointPropertyFilter {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected EStructuralFeature getFeature() {
+		return PropertiesPackage.eINSTANCE.getTextWidgetStyle_FontFormat();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	protected boolean isRightInputType(Object arg0) {
+        return arg0 instanceof org.eclipse.sirius.properties.TextWidgetStyle;
+    }
+	
+	// Start of user code user methods
+
+    // End of user code user methods
+	
+	
+
+}
