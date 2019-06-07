6c654550122c7843df99307d01c73329adf744b8
Pierre-Charles David <pierre-charles.david@obeo.fr>
2019-06-07 07:46:48 +0200

    [cleanup] Cleanup DViewHelper
    Change-Id: I186fc1ab3cd5f81b37c55fe7f268b01e28d08d8d
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DViewHelper.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DViewHelper.java
@@ -12,20 +12,18 @@
  *******************************************************************************/
 package org.eclipse.sirius.business.internal.session.danalysis;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
 import org.eclipse.sirius.business.api.dialect.DialectManager;
 import org.eclipse.sirius.business.api.query.DViewQuery;
 import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.ecore.extender.business.api.accessor.ModelAccessor;
 import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.DView;
-import org.eclipse.sirius.viewpoint.SiriusPlugin;
 
 /**
  * Helper operations on {@link DView}.
@@ -44,33 +42,24 @@ public final class DViewHelper {
      *            the view to refresh.
      */
     public static void refreshViewContents(DView self) {
-        final Set<DRepresentation> representationsToDelete = new HashSet<DRepresentation>();
-        Iterator<DRepresentation> it = new DViewQuery(self).getLoadedRepresentations().iterator();
-        while (it.hasNext()) {
-            final DRepresentation representation = it.next();
-            /*
-             * detect dangling representations.
-             */
-            if (representation instanceof DSemanticDecorator && ((DSemanticDecorator) representation).getTarget() == null) {
+        final Set<DRepresentation> representationsToDelete = new LinkedHashSet<>();
+        for (DRepresentation representation : new DViewQuery(self).getLoadedRepresentations()) {
+            if (isDangling(representation)) {
                 representationsToDelete.add(representation);
-            }
-            if (!representationsToDelete.contains(representation)) {
+            } else {
                 DialectManager.INSTANCE.refresh(representation, new NullProgressMonitor());
             }
         }
-        /*
-         * delete dangling representations
-         */
-        it = representationsToDelete.iterator();
-        while (it.hasNext()) {
-            final EObject next = it.next();
-            final Session session;
-            if (next instanceof DSemanticDecorator) {
-                session = SessionManager.INSTANCE.getSession(((DSemanticDecorator) next).getTarget());
-            } else {
-                session = SessionManager.INSTANCE.getSession(next);
+        Session.of(self).ifPresent(session -> {
+            ModelAccessor ma = session.getModelAccessor();
+            ECrossReferenceAdapter xref = session.getSemanticCrossReferencer();
+            for (DRepresentation dangling : representationsToDelete) {
+                ma.eDelete(dangling, xref);
             }
-            SiriusPlugin.getDefault().getModelAccessorRegistry().getModelAccessor(next).eDelete(next, session != null ? session.getSemanticCrossReferencer() : null);
-        }
+        });
+    }
+
+    private static boolean isDangling(DRepresentation representation) {
+        return representation instanceof DSemanticDecorator && ((DSemanticDecorator) representation).getTarget() == null;
     }
 }
